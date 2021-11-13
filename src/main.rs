99fb5c58e8689b7a3610ef67334cd7e09f19af94
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2021-11-13 18:01:50 +0100

    Switch database driver to sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use axum::{
     async_trait,
     extract::{Extension, FromRequest, RequestParts},
@@ -5,37 +6,42 @@ use axum::{
     routing::get,
     AddExtensionLayer, Router,
 };
-use bb8::{Pool, PooledConnection};
-use bb8_postgres::PostgresConnectionManager;
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::PgPool;
+use std::env;
 use std::net::SocketAddr;
-use tokio_postgres::NoTls;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
+    dotenv()?;
+
     // Set the RUST_LOG, if it hasn't been explicitly defined
     if std::env::var_os("RUST_LOG").is_none() {
         std::env::set_var("RUST_LOG", "bercik_server=debug")
     }
+
+    // Initialize tracing
     tracing_subscriber::fmt::init();
 
-    // setup connection pool
-    let mut config = tokio_postgres::Config::new();
-    config
-        .user("postgres")
-        .password("example")
-        .host("localhost");
-    let _ = config.connect(NoTls).await.unwrap();
+    // Setup connection pool
+    let database_url = env::var("DATABASE_URL").context("Database url not in .env file")?;
+    let db_pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
 
-    let manager = PostgresConnectionManager::new(config, NoTls);
-    let pool = Pool::builder().build(manager).await.unwrap();
+    // Make sure database is working
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&db_pool)
+        .await?;
+    assert_eq!(row.0, 150);
 
     // build our application with some routes
     let app = Router::new()
-        .route(
-            "/",
-            get(using_connection_pool_extractor).post(using_connection_extractor),
-        )
-        .layer(AddExtensionLayer::new(pool));
+        .route("/", get(endpoint_test))
+        .layer(AddExtensionLayer::new(db_pool));
 
     // run it with hyper
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
@@ -44,64 +50,16 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    Ok(())
 }
 
-type ConnectionPool = Pool<PostgresConnectionManager<NoTls>>;
-
-// we can exact the connection pool with `Extension`
-async fn using_connection_pool_extractor(
-    Extension(pool): Extension<ConnectionPool>,
-) -> Result<String, (StatusCode, String)> {
-    let conn = pool.get().await.map_err(internal_error)?;
-
-    let row = conn
-        .query_one("select 1 + 1", &[])
+async fn endpoint_test(Extension(pool): Extension<PgPool>) -> std::result::Result<String, String> {
+    let row: (i64,) = sqlx::query_as("SELECT $1 + $1")
+        .bind(150_i64)
+        .fetch_one(&pool)
         .await
-        .map_err(internal_error)?;
-    let two: i32 = row.try_get(0).map_err(internal_error)?;
+        .map_err(|x| format!("{:?}", x))?;
 
-    Ok(two.to_string())
-}
-
-// we can also write a custom extractor that grabs a connection from the pool
-// which setup is appropriate depends on your application
-struct DatabaseConnection(PooledConnection<'static, PostgresConnectionManager<NoTls>>);
-
-#[async_trait]
-impl<B> FromRequest<B> for DatabaseConnection
-where
-    B: Send,
-{
-    type Rejection = (StatusCode, String);
-
-    async fn from_request(req: &mut RequestParts<B>) -> Result<Self, Self::Rejection> {
-        let Extension(pool) = Extension::<ConnectionPool>::from_request(req)
-            .await
-            .map_err(internal_error)?;
-
-        let conn = pool.get_owned().await.map_err(internal_error)?;
-
-        Ok(Self(conn))
-    }
-}
-
-async fn using_connection_extractor(
-    DatabaseConnection(conn): DatabaseConnection,
-) -> Result<String, (StatusCode, String)> {
-    let row = conn
-        .query_one("select 1 + 1", &[])
-        .await
-        .map_err(internal_error)?;
-    let two: i32 = row.try_get(0).map_err(internal_error)?;
-
-    Ok(two.to_string())
-}
-
-/// Utility function for mapping any error into a `500 Internal Server Error`
-/// response.
-fn internal_error<E>(err: E) -> (StatusCode, String)
-where
-    E: std::error::Error,
-{
-    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
+    Ok(format!("{}", row.0))
 }
